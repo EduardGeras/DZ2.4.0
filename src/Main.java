db65a6b17b693f5562dc9c9ccd755021d8229ca7
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-06-29 01:21:38 +0500

    Нужна подсказка)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException{
+        boolean data = PossibleErrors.possibleErrors(new String[]{"java_skypro.go"},
+                new String[]{"D_1hWiKjjP_9"},
+                new String[]{"D_1hWiKjjP_9"});
+        if (data) {
+            System.out.println("Логин и пароль корректные");
+        } else {
+            System.out.println("Ой! Что-то пошло не так :(");
+        }
     }
 }
